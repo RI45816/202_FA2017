8f0450c44e69093b8012f86f8239c1a9f4b876ad
sirius16 <amozu16@gmail.com>
2017-10-16 11:00:45 -0400

    @90819@

--- Labs/lab6/lab6.cpp
+++ Labs/lab6/lab6.cpp
@@ -14,26 +14,44 @@ void printVector(vector<Student>&);
 // @param const vector<Student>& students in class
 // Passed by reference
 
-int main () {
-  //Creates a new vector of Students
-  vector<Student> myClass;
-  //Populates the vector
-  fillVector(myClass);
-  //Prints the contents of the vector
-  printVector(myClass);
+int main() {
+    //Creates a new vector of Students
+    vector<Student> myClass;
+    //Populates the vector
+//    fillVector(myClass);
+    //Prints the contents of the vector
+//    printVector(myClass);
 
-  return 0;
+    return 0;
 }
 
 void fillVector(vector<Student>& newMyClass) {
-  //Write this function
-  //Needs to ask how many students
-  //Gets first name, last name, and ID of a student
-  //Uses the overloaded constructor for the Student
-  //Needs to add student to the vector
-
+    //Write this function
+    //Needs to ask how many students
+    //Gets first name, last name, and ID of a student
+    //Uses the overloaded constructor for the Student
+    //Needs to add student to the vector
+    int numStudents, studentID;
+    string firstName, lastName;
+    cout << "How many students are in your class?:" << endl;
+    cin >> numStudents;
+    for (int i = 0; i < numStudents; i++) {
+        cout << "Student " << i + 1 << endl;
+        cout << "Student First Name:" << endl;
+        cin >> firstName;
+        cout << "Student Last Name:" << endl;
+        cin >> lastName;
+        cout << "Student ID:" << endl;
+        cin >> studentID;
+        newMyClass.push_back(Student(firstName, lastName, studentID));
+    }
 }
 
-void printVector(vector<Student>& newMyClass){
-  //Needs to loop through the vector of Students and outputs the information
+void printVector(vector<Student>& newMyClass) {
+    for (int i = 0; i < newMyClass.size(); i++) {
+        cout << "Student First Name: " << newMyClass[i].GetFirstName() << endl;
+        cout << "Student Last Name: " << newMyClass[i].GetLastName() << endl;
+        cout << "Student ID: " << newMyClass[i].GetID() << endl << endl;
+
+    }
 }
