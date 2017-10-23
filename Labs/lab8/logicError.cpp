38d4e3484700c6b22e7627504287258f2512eed2
RI45816 <uu3@umbc.edu>
2017-10-23 11:10:49 -0400

    8ZV1R

---
+++ Labs/lab8/logicError.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int ComputeFactorial(int number);
+double ComputeSeriesValue(double x, int n);
+
+int main() {
+  double x, n, seriesValue;
+  cout << "This program computes the value of the following series : " << endl;
+  cout << "(x^0)/0! + (x^1)/1! + (x^2)/2! + (x^3)/3! + ........ + (x^n)/n! " << endl;
+  cout << "Please enter the value of x : " ;
+  cin >> x;
+
+  cout << endl << "Please enter an integer value for n : " ;
+  cin >> n;
+  cout << endl;
+
+  seriesValue = ComputeSeriesValue(x, n);
+  cout << "The value of the series for the values entered is " 
+       << seriesValue << endl;
+
+  return 0;
+}
+
+int ComputeFactorial(int number) {
+  int fact = 0;
+
+  for (int j = 1; j <= number; j++) {
+    fact = fact * j;
+  }
+  return fact;
+}
+
+double ComputeSeriesValue(double x, int n) {
+  double seriesValue = 0.0;
+  double xpow = 1;
+
+  for (int k = 0; k <= n; k++) {
+    seriesValue += xpow / ComputeFactorial(k);
+    xpow = xpow * x;
+  }
+  return seriesValue;
+}
