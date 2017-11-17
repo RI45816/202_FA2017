97e3977ff326d1fb77ecf97cc148f878541c7cf1
sirius16 <amozu16@gmail.com>
2017-11-16 20:14:52 -0500

    @55100@

--- Projects/proj4/main.cpp
+++ Projects/proj4/main.cpp
@@ -59,6 +59,8 @@ int main(int argc, char** argv) {
             cout << (!turnIndicator ? "You won!" : "Computer has won") << endl;
             delete grids[0];
             delete grids[1];
+            delete players[0];
+            delete players[1];
             return 0;
         }
         }
