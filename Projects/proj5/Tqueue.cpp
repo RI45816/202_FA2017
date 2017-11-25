61510432d1e1d64a30737db6ee65c133cde438d9
RI45816 <uu3@umbc.edu>
2017-11-24 19:06:15 -0500

    KC01S

---
+++ Projects/proj5/Tqueue.cpp
@@ -0,0 +1,63 @@
+#ifndef TQUEUE_H
+#define TQUEUE_H
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+template <class T, int N>
+class Tqueue {
+public:
+  //Name: Tqueue - Default Constructor
+  //Precondition: None (Must be templated)
+  //Postcondition: Creates a queue using dynamic array
+  Tqueue(); //Default Constructor
+  //Name: Copy Constructor
+  //Precondition: Existing Tqueue
+  //Postcondition: Copies an existing Tqueue
+  Tqueue( const Tqueue<T,N>& x ); //Copy Constructor
+  //Name: Destructor
+  //Precondition: Existing Tqueue
+  //Postcondition: Destructs existing Tqueue
+  ~Tqueue(); //Destructor
+  //Name: enqueue
+  //Precondition: Existing Tqueue
+  //Postcondition: Adds item to back of queue
+  void enqueue(T data); //Adds item to queue (to back)
+  //Name: dequeue
+  //Precondition: Existing Tqueue
+  //Postcondition: Removes item from front of queue
+  void dequeue(T &); //Removes item from queue (from front)
+  //Name: queueFront
+  //Precondition: Existing Tqueue
+  //Postcondition: Retrieve front (does not remove it)
+  void queueFront(T &);    // Retrieve front (does not remove it)
+  //Name: isEmpty
+  //Precondition: Existing Tqueue
+  //Postcondition: Returns 1 if queue is empty else 0
+  int isEmpty(); //Returns 1 if queue is empty else 0
+  //Name: isFull
+  //Precondition: Existing Tqueue
+  //Postcondition: Returns 1 if queue is full else 0
+  int isFull(); //Returns 1 if queue is full else 0
+  //Name: size
+  //Precondition: Existing Tqueue
+  //Postcondition: Returns size of queue
+  int size(); //Returns size of queue
+  //Name: Overloaded assignment operator
+  //Precondition: Existing Tqueue
+  //Postcondition: Sets one Tqueue to same as a second Tqueue using =
+  Tqueue<T,N>& operator=( Tqueue<T,N> y); //Overloaded assignment operator for queue
+  //Name: Overloaded [] operator
+  //Precondition: Existing Tqueue
+  //Postcondition: Returns object from Tqueue using []
+  T& operator[] (int x);//Overloaded [] operator to pull data from queue
+private:
+  T* m_data; //Data of the queue (Must be dynamically allocated array)
+  int m_front; //Front of the queue
+  int m_back; //Back of the queue
+};
+
+//**** Add class definition below ****
+
+#endif
